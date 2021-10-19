7acdf1973ca3f3fec5d69d7464f20c76346fcb0a
Niklas Mollenhauer <nikeee@outlook.com>
2021-10-20 01:23:13 +0200

    Implement necessary methods for integration

--- packages/hurl/src/runner/fs_context.rs
+++ packages/hurl/src/runner/fs_context.rs
@@ -1,9 +1,13 @@
-
-use std::io::Read;
+use std::fs::File;
+use std::io::{Read, Result};
 use std::path::Path;
 
+use hurl_core::ast::Filename;
+
 pub trait DirectoryContext<R: Read> /* : Clone */ {
-    fn open(&self, absolute_filename: &Path) -> Option<R>;
+    fn exists(&self, filename: Filename) -> bool;
+    fn open(&self, filename: Filename) -> Result<R>;
+    fn get_absolute_filename(&self, filename: Filename) -> String;
 }
 
 #[derive(Debug, Clone)]
@@ -12,8 +16,8 @@ pub struct FsDirectoryContext<R: Read> {
     base_dir: String,
 }
 
-impl FsDirectoryContext<std::fs::File> {
-    pub fn new(base_dir: String) -> FsDirectoryContext<std::fs::File> {
+impl FsDirectoryContext<File> {
+    pub fn new(base_dir: String) -> FsDirectoryContext<File> {
         FsDirectoryContext {
             resource_type: std::marker::PhantomData,
             base_dir,
@@ -21,8 +25,31 @@ impl FsDirectoryContext<std::fs::File> {
     }
 }
 
-impl<R: Read> DirectoryContext<R> for FsDirectoryContext<R> {
-    fn open(&self, absolute_filename: &Path) -> Option<R> {
-        None
+impl<R: Read> DirectoryContext<File> for FsDirectoryContext<R> {
+    fn exists(&self, filename: Filename) -> bool {
+        let absolute_filename = self.get_absolute_filename(filename);
+
+        Path::new(&absolute_filename).exists()
+    }
+
+    fn open(&self, filename: Filename) -> Result<File> {
+        let absolute_filename = self.get_absolute_filename(filename);
+
+        File::open(absolute_filename.clone())
+    }
+
+    fn get_absolute_filename(&self, filename: Filename) -> String {
+        let f = filename.value.as_str();
+        let path = Path::new(f);
+
+        if path.is_absolute() {
+            filename.value
+        } else {
+            Path::new(self.base_dir.as_str())
+                .join(f)
+                .to_str()
+                .unwrap()
+                .to_string()
+        }
     }
 }
